b4829849c77083125826dac5b90db672da18c510
hzlloni <jhw030329@gmail.com>
2024-06-08 02:48:55 +0900

    complete ml_kit

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,5 +1,7 @@
 import 'package:everyplogging/login.dart';
 import 'package:flutter/material.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -12,9 +14,10 @@ class Signup extends StatefulWidget {
 
 class _SignupState extends State<Signup> {
   final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _emailController = TextEditingController(); 
+  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _confirmPasswordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
   final TextEditingController _schoolController = TextEditingController();
   final TextEditingController _birthController = TextEditingController();
 
@@ -41,7 +44,8 @@ class _SignupState extends State<Signup> {
       setState(() {
         _isDuplicate = snapshot.docs.isNotEmpty;
         _isCheckingDuplicate = false;
-        _duplicateCheckMessage = _isDuplicate ? '중복된 아이디입니다.' : '사용 가능한 아이디입니다.';
+        _duplicateCheckMessage =
+            _isDuplicate ? '중복된 아이디입니다.' : '사용 가능한 아이디입니다.';
       });
     } catch (e) {
       print('Error checking duplicate: $e');
@@ -114,10 +118,11 @@ class _SignupState extends State<Signup> {
               'assets/signup.png',
               fit: BoxFit.fill,
               width: double.infinity,
-              height: 290, 
+              height: 290,
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
               child: Column(
                 children: [
                   TextField(
@@ -145,7 +150,8 @@ class _SignupState extends State<Signup> {
                       );
                       if (pickedDate != null) {
                         setState(() {
-                          _birthController.text = DateFormat('yyyy-MM-dd').format(pickedDate);
+                          _birthController.text =
+                              DateFormat('yyyy-MM-dd').format(pickedDate);
                         });
                       }
                     },
@@ -160,17 +166,18 @@ class _SignupState extends State<Signup> {
                             border: OutlineInputBorder(),
                             labelText: '아이디',
                           ),
-                          keyboardType: TextInputType.emailAddress, 
+                          keyboardType: TextInputType.emailAddress,
                         ),
                       ),
                       const SizedBox(width: 10),
                       ElevatedButton(
-                        onPressed: _isCheckingDuplicate ? null : _checkDuplicateEmail,
+                        onPressed:
+                            _isCheckingDuplicate ? null : _checkDuplicateEmail,
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.grey[200], 
-                          foregroundColor: Colors.black, 
+                          backgroundColor: Colors.grey[200],
+                          foregroundColor: Colors.black,
                           shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(0), 
+                            borderRadius: BorderRadius.circular(0),
                           ),
                         ),
                         child: _isCheckingDuplicate
@@ -229,14 +236,46 @@ class _SignupState extends State<Signup> {
                       ),
                       const SizedBox(width: 10),
                       ElevatedButton(
-                        onPressed: () {
+                        onPressed: () async {
+                          // 이미지 피커 인스턴스 생성
+                          final picker = ImagePicker();
 
+                          // 이미지를 갤러리에서 선택
+                          final pickedFile = await picker.pickImage(
+                              source: ImageSource.gallery);
+
+                          if (pickedFile != null) {
+                            // 이미지 파일로부터 입력 이미지 생성
+                            final inputImage =
+                                InputImage.fromFilePath(pickedFile.path);
+
+                            // 텍스트 인식기 인스턴스 생성
+                            final textRecognizer = TextRecognizer();
+
+                            // 텍스트 인식 수행
+                            final RecognizedText recognizedText =
+                                await textRecognizer.processImage(inputImage);
+
+                            // 인식된 텍스트를 터미널에 출력
+                            print(recognizedText.text);
+
+                            // '한동대'라는 글자가 포함되어 있는지 확인하고 텍스트 필드에 설정
+                            if (recognizedText.text.contains('한동대') ||
+                                recognizedText.text.contains('HANDONG')) {
+                              setState(() {
+                                _schoolController.text = '한동대';
+                              });
+                            }
+
+                            // 텍스트 인식기 해제
+                            textRecognizer.close();
+                          }
                         },
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.grey[200], 
-                          foregroundColor: Colors.black, 
+                          backgroundColor: Colors.grey[200],
+                          foregroundColor: Colors.black,
                           shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(0), 
+                            borderRadius: BorderRadius.circular(0),
                           ),
                         ),
                         child: const Icon(Icons.camera_alt),
@@ -258,16 +297,38 @@ class _SignupState extends State<Signup> {
                     child: ElevatedButton(
                       onPressed: _isRegistering ? null : _saveUserData,
                       style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF7CC0FF), 
-                        foregroundColor: Colors.white, 
+                        backgroundColor: const Color(0xFF7CC0FF),
+                        foregroundColor: Colors.white,
                         shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(0), 
+                          borderRadius: BorderRadius.circular(0),
                         ),
                         padding: const EdgeInsets.symmetric(vertical: 15),
                       ),
                       child: _isRegistering
                           ? const CircularProgressIndicator(color: Colors.white)
                           : const Text('회원가입 하기'),
+                    ),
+                  ),
+                  const SizedBox(height: 10),
+                  SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const Login()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(3),
+                        ),
+                        padding: const EdgeInsets.symmetric(vertical: 15),
+                      ),
+                      child: const Text('로그인 하러 하기'),
                     ),
                   ),
                 ],
